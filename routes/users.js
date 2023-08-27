08884987b2160bf89dc8d9f2d13170827d01f02b
HJQ <17774657825@163.com>
2023-08-28 01:34:08 +0800

    添加增删改查代码

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
+const UserGroup = [];
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json(UserGroup)
+});
+router.post('/', function(req, res, next) {
+  if(!req.body.name) return res.send('请填写name');
+  else if(UserGroup.find(v => v.name == req.body.name)) return res.send('该用户已存在');
+  else {
+    UserGroup.push(req.body);
+    res.send('success!');
+  }
+});
+router.delete('/', function(req, res, next) {
+  if(!req.query.name) return res.send('请填写name');
+  else if(!UserGroup.find(v => v.name == req.query.name)) return res.send('该用户不存在');
+  else {
+    let index = UserGroup.findIndex(v => v.name == req.query.name);
+    UserGroup.splice(index, 1);
+    res.send(JSON.stringify(UserGroup));
+  }
+});
+router.put('/', function(req, res, next) {
+  if(!req.body.name) return res.send('请填写name');
+  else if(!UserGroup.find(v => v.name == req.body.name)) return res.send('该用户不存在');
+  else {
+    let index = UserGroup.findIndex(v => v.name == req.body.name);
+    UserGroup[index] = req.body;
+    res.json(UserGroup)
+  }
 });
 
 module.exports = router;
