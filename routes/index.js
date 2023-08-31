5ea20d678f21530177a098be8af6f470bf89e398
HJQ <17774657825@163.com>
2023-09-01 00:54:56 +0800

    test mongodb

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* 查 */
+router.all('/all',function(req,res){
+  // 查询操作
+  infoModel.find({}).exec(function(err,data){
+      if(err){
+          res.send("查询异常");
+      }else{
+          console.log("查询成功");
+          res.send(data);
+      }
+  })
+})
 module.exports = router;
