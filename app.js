015f4a68a80bf126c8bf572d89308a3bef881e70
HJQ <17774657825@163.com>
2023-09-10 04:11:32 +0800

    更新

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-const config = require("./global.config")['development']
+const config = require("./global.config")['production']
 global.CONF = config;
 
 var app = express();
